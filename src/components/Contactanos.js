b4d728548b92ef698c02f4e7c491e6f97d195747
Wixent <wixent.a@gmail.com>
2023-03-07 01:13:58 -0300

    asd

--- src/components/Contactanos.js
+++ src/components/Contactanos.js
@@ -1,19 +1,33 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useForm } from 'react-hook-form';
 
 import hr from "../assets/curve-hr.svg"
 
 export default function Contactanos() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+    
 
     return (
         <div id="contact" className="mt-4 text-white">
+            
             <h1 className="text-2xl font-bold">Contactanos</h1>
             <p className="font-light text-gray-400">Rellena el formulario para hablarnos de manera privada sobre cualquier duda, sugerencia o propuesta.</p>
 
+       
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Nombre" {...register("Nombre", {})} />
+            <input type="email" placeholder="Mail" {...register("Mail", {})} />
+            <textarea {...register("Mensaje", {})} />
 
+         < input type="submit" />
+            </form>
         
             <img src={hr} className="w-full mt-8 md:h-3" alt="hr" />
         </div>
+         
     )
 }
